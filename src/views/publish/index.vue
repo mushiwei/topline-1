0741f144365e8cc7af525c908f0e0c6bdf88a37a
mushiweiit <mushiweiit@aliyun.com>
2019-06-29 16:32:36 +0800

    富文本编辑器的使用

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -8,20 +8,25 @@
       </div>
     </div>
     <el-row>
-      <el-col :span="10">
+      <el-col :span="16">
         <!-- 表单 -->
         <el-form ref="form" :model="articleForm" label-width="80px">
           <el-form-item label="标题">
             <el-input v-model="articleForm.title"></el-input>
           </el-form-item>
           <el-form-item label="内容">
-            <el-input type="textarea" v-model="articleForm.content"></el-input>
+            <!-- bidirectional data binding（双向数据绑定） 查阅github说明 使用富文本编辑器 SPA项  双向绑定articleForm.content-->
+            <quill-editor
+            v-model="articleForm.content"
+              ref="myQuillEditor"
+              :options="editorOption"
+            ></quill-editor>
           </el-form-item>
           <el-form-item label="封面">
             <!-- <el-radio-group>
               <el-radio label="线上品牌商赞助"></el-radio>
               <el-radio label="线下场地免费"></el-radio>
-            </el-radio-group> -->
+            </el-radio-group>-->
           </el-form-item>
           <el-form-item label="频道">
             <!--
@@ -30,17 +35,17 @@
                 绑定一个名字叫 value 的数据给子组件
               v-on:input="articleForm.channel_id = $event"
                 默认监听子组件的 input 自定义事件，事件发生以后，将事件参数赋值给你绑定的数据
-             -->
+            -->
             <article-channel v-model="articleForm.channel_id"></article-channel>
             <!--
               在组件上 v-model
                 数据会影响视图
                 视图也会影响数据
-             -->
+            -->
             <!-- <el-select v-model="articleForm.channel_id" placeholder="请选择活动区域">
               <el-option label="区域一" value="shanghai"></el-option>
               <el-option label="区域二" value="beijing"></el-option>
-            </el-select> -->
+            </el-select>-->
           </el-form-item>
         </el-form>
         <!-- /表单 -->
@@ -51,23 +56,33 @@
 
 <script>
 import ArticleChannel from '@/components/article-channel'
+// 引入第三方富文本编辑器
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
 
 export default {
   name: 'AppPublish',
   components: {
-    ArticleChannel
+    ArticleChannel,
+    // 注册富文本编辑器
+    quillEditor
   },
   data () {
     return {
       articleForm: {
         title: '', // 标题
         content: '', // 内容
-        channel_id: 3, // 频道
-        cover: { // 封面
+        channel_id: '', // 频道
+        cover: {
+          // 封面
           type: 0, // 封面类型 -1:自动，0-无图，1-1张，3-3张
           images: []
         }
-      }
+      },
+      // 富文本编辑器配置选项
+      editorOption: {}
     }
   },
 
